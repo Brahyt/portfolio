f37b22d85f50639fe15238ef2a3165b298514848
Daniel Bright <brahyt@gmail.com>
2019-10-10 17:28:15 -0700

    added alt text to images from store

--- store.js
+++ store.js
@@ -4,21 +4,24 @@ const tinkeringStuff = [
   liveLink: 'https://thinkful-ei-heron.github.io/bookmark-app-danielb/',
   githubLink: 'https://github.com/thinkful-ei-heron/bookmark-app-danielb',
   desc: 'Bookmarking App that utilizes Jquery and Javascript with communicate with an API to store and manage data in the form of a bookmark. ',
-  preview: 'images/bookmarks.png'
+  preview: 'images/bookmarks.png',
+  alt: 'Screenshot of the a bookmarking app that I made in Jquery and Javascript.'
   },
   {
   title: 'Quiz Hunter',
   liveLink: 'https://thinkful-ei-heron.github.io/Mike-DanielB-quiz-hunter/',
   githubLink: 'https://github.com/thinkful-ei-heron/Mike-DanielB-quiz-hunter/',
   desc: 'A singlepage Javascript app that utilized jquery, HTML and CSS to create a quiz app',
-  preview: 'images/qhunt.png'
+  preview: 'images/qhunt.png',
+  alt: 'Image of an app I call quiz hunter.  I built this in Javascript and HTML with CSS.'
   },
   {
   title: 'Chalmun\'s Cantina',
   liveLink: 'https://www.url.com',
   githubLink: 'https://github.com/hone/chalmuns-cantina',
   desc: 'Web app that is built using Ember and Javascript for Star Wars: Outter Rim.',
-  preview: 'images/chaul.png'
+  preview: 'images/chaul.png',
+  alt: 'Image of an app I call Chalmuns Cantina. I built it in Ember.'
   }
 ]
 
@@ -26,7 +29,7 @@ const tinkerSnippit = function(tinkerObj) {
   return `
     <div class="tinker-item">
       <div>
-        <img class="tinker-image" src="${tinkerObj.preview}">
+        <img class="tinker-image" src="${tinkerObj.preview}" alt="${tinkerObj.alt}">
       </div>
       <div>
         <div class="tinker-title">
