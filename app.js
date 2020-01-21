2bfa0b10c8c6225ea28b2eed01f8b597f3da47ff
Daniel Bright <brahyt@gmail.com>
2020-01-21 07:43:32 -0800

    fixing buttons

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
-import store from './store.js'
+import store from './store.js';
 
-
-store.populateTinkering()
+store.populateTinkering();
 
 const myNav = document.querySelector('nav');
 const myDoc = document.querySelector('body');
 
-window.onscroll = function () { 
-		console.log(window.pageYOffset)
-    if (window.pageYOffset >= 220 ) {
-        myNav.classList.add("nav-colored");
-        myNav.classList.remove("nav-trans");
-    } 
-    else {
-        myNav.classList.remove("nav-colored");
-        myNav.classList.add("nav-trans");
-    }
+window.onscroll = function() {
+  console.log(window.pageYOffset);
+  if (window.pageYOffset >= 220) {
+    myNav.classList.add('nav-colored');
+    myNav.classList.remove('nav-trans');
+  } else {
+    myNav.classList.remove('nav-colored');
+    myNav.classList.add('nav-trans');
+  }
 };
--- store.js
+++ store.js
@@ -1,24 +1,27 @@
 const tinkeringStuff = [
   {
-  title: 'BookMarking App',
-  liveLink: 'https://thinkful-ei-heron.github.io/bookmark-app-danielb/',
-  githubLink: 'https://github.com/thinkful-ei-heron/bookmark-app-danielb',
-  desc: 'Bookmarking App that utilizes Jquery and Javascript with communicate with an API to store and manage data in the form of a bookmark. ',
+  title: 'Dragon Fire app',
+  liveLink: 'https://www.weyr-alliance.com/',
+  githubLink1: 'https://github.com/thinkful-ei-heron/bookmark-app-danielb',
+  githubLink2: '',
+  desc: 'A party managment tool for the card game Dragon Fire.  Built in React, Node/Express, utilizing a postgresql database.',
   preview: 'images/bookmarks.png',
   alt: 'Screenshot of the a bookmarking app that I made in Jquery and Javascript.'
   },
   {
   title: 'Quendi - Spaced Repetition',
   liveLink: 'https://www.quendi.app',
-  githubLink: 'https://github.com/thinkful-ei-heron/Spaced-Rep-Andrea-DanB-Server',
-  desc: 'Full stack Javascript web app built in React, Express and Node  utilizing Linked List data structure and a SQL database',
+  githubLink1: 'https://github.com/thinkful-ei-heron/Spaced-Rep-Andrea-DanB-Server',
+  githubLink2: '',
+  desc: 'Full stack Javascript web app built in React, Express and Node utilizing Linked List data structure and a SQL database',
   preview: 'images/qhunt.png',
   alt: 'Green background website with lists of words to learn.'
   },
   {
   title: 'Danger Room',
   liveLink: 'https://danger-room.herokuapp.com/situation',
-  githubLink: 'https://github.com/hone/danger-room',
+  githubLink1: 'https://github.com/hone/danger-room',
+  githubLink2: '',
   desc: 'Web app that is built using Ember.js and Javascript for the Marvel Champions Card Game',
   preview: 'images/chaul.png',
   alt: 'Website screenshot of Danger Room.'
